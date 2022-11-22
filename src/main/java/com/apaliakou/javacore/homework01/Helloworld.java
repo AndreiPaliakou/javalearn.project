8d2572df63a319923b81f2250f1b89d45e9ff8f2
AndreiPaliakou <AndreiPaliakouBelarus@gmail.com>
2022-11-23 01:14:09 +0300

    Hello world

---
+++ src/main/java/com/apaliakou/javacore/homework01/Helloworld.java
@@ -0,0 +1,7 @@
+package main.java.com.apaliakou.javacore.homework01;
+
+public class Helloworld {
+    public static void main(String[] args) {
+        System.out.println("Hello world!!!!!!!!!!!!!");
+    }
+}
