340c4343af2e226cfc88ae5c9666eeeec48fae5c
AndreiPaliakou <AndreiPaliakouBelarus@gmail.com>
2022-11-25 11:22:18 +0300

    It was changed m1/m2-parse-operators, added Math.pow function, formula was divided into two parts, added if-condition

--- src/Exercise2.java
+++ src/Exercise2.java
@@ -1,20 +1,20 @@
 public class Exercise2 {
     public static void main(String[] args) {
-        System.out.println("Hello world!!!");
-        System.out.println("");
-        System.out.println("");
+        System.out.println("Hello world!!!\n\n");
         for(int i = 0; i < args.length; i++) {
-            System.out.println("Аргумент № " + i + ": args [" + i + "] = " + args [i] + "    ");
-        };
-        System.out.println("");
-        System.out.println("");
-        double G;
+            System.out.println("Аргумент № " + i + ": args [" + i + "] = " + args [i] + "    \n\n");
+        }
+        double g;
         final double Pi = 3.14;
         int a = Integer.parseInt(args[0]);
         int p = Integer.parseInt(args[1]);
-        double m1 = Integer.parseInt(args[2]);
-        double m2 = Integer.parseInt(args[3]);
-        G = (4*Pi*Pi*a*a*a/p/p)/(m1+m2);
-        System.out.println("Ответ: G =  " + G + ".");
+        double m1 = Double.parseDouble(args[2]);
+        double m2 = Double.parseDouble(args[3]);
+        double degree = Math.pow(a, 3);
+        double up = 4*Pi*Pi*degree;
+        double down = p*p*(m1+m2);
+        if (down != 0) {g = up/down;
+        System.out.println("Ответ: g =  " + g + ".");
+        }
     }
 }
