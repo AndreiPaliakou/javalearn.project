ba1418d824c1fb7f5dcf8a9b292c5c487f08b945
Andrei <117518981+AndreiPaliakou@users.noreply.github.com>
2022-11-28 23:10:23 +0300

    It was changed m1/m2-parse-operators, added Math.pow function, formula was divided into two parts, added if-condition (#3)

--- src/Exercise2.java
+++ src/Exercise2.java
@@ -5,13 +5,12 @@ public class Exercise2 {
             System.out.println("Аргумент № " + i + ": args [" + i + "] = " + args [i] + "    \n\n");
         }
         double g;
-        final double Pi = 3.14;
         int a = Integer.parseInt(args[0]);
         int p = Integer.parseInt(args[1]);
         double m1 = Double.parseDouble(args[2]);
         double m2 = Double.parseDouble(args[3]);
         double degree = Math.pow(a, 3);
-        double up = 4*Pi*Pi*degree;
+        double up = 4*Math.PI*Math.PI*degree;
         double down = p*p*(m1+m2);
         if (down != 0) {g = up/down;
         System.out.println("Ответ: g =  " + g + ".");
