b3178c7bcaaf2cfb0fa23a256cb4d86f8d24aa36
AndreiPaliakou <AndreiPaliakouBelarus@gmail.com>
2023-01-25 22:55:43 +0300

    Add median task solution

--- src/main/java/com/apaliakou/homework03/median/Median.java
+++ src/main/java/com/apaliakou/homework03/median/Median.java
@@ -1,25 +1,29 @@
 package main.java.com.apaliakou.homework03.median;
 
+import java.util.Arrays;
+
 public class Median {
     private Median() {
     }
 
     public static int median(int[] array) {
-        int approximateIntArrayMedian = array.length / 2;
-        for (int x = 0; x < array.length; x++) {
-            if (x <= approximateIntArrayMedian) {
-                return array[approximateIntArrayMedian];
+        Arrays.sort(array);
+        if (array.length % 2 != 0) {
+            return array[array.length / 2];
+        }
+        return (array[array.length / 2 - 1] + array[array.length / 2]) / 2;
+    }
+
+    public static double median(double[] sortedArray) {
+        int approximateMiddle = sortedArray.length / 2;
+        int result = 0;
+        for (int x = 0; x < sortedArray.length; x++) {
+            if ((sortedArray.length % 2 != 0) && (x == approximateMiddle)) {
+                return sortedArray[approximateMiddle];
+            } else if ((sortedArray.length % 2 == 0) && (x == (approximateMiddle) - 1)) {
+                return sortedArray[0] = (sortedArray[x] + sortedArray[x + 1]) / 2;
             }
         }
-        return -1;
+        return result;
     }
-        public static double median (double[] array) {
-            int approximateDoubleArrayMedian = array.length / 2;
-            for (int i = 0; i < array.length; i++) {
-                if (i <= approximateDoubleArrayMedian) {
-                    return array[approximateDoubleArrayMedian];
-                }
-            }
-            return -1;
-        }
-    }
+}
