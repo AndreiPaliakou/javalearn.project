6c7c288260598e9171c1122d78563d2bd13ef867
AndreiPaliakou <AndreiPaliakouBelarus@gmail.com>
2023-05-05 22:50:54 +0300

    Task14 - Client-server request-response(version 5)

--- src/main/java/com/apaliakou/homework14/model/impl/httpClient/HttpClientConnection.java
+++ src/main/java/com/apaliakou/homework14/model/impl/httpClient/HttpClientConnection.java
@@ -5,9 +5,7 @@ import com.apaliakou.homework14.model.WebClient;
 import com.sun.net.httpserver.HttpHandlers;
 import org.apache.http.HttpHeaders;
 import org.apache.http.HttpResponse;
-import org.apache.http.client.methods.CloseableHttpResponse;
-import org.apache.http.client.methods.HttpGet;
-import org.apache.http.client.methods.HttpUriRequest;
+import org.apache.http.client.methods.*;
 import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClients;
 import org.codehaus.classworlds.uberjar.protocol.jar.Handler;
@@ -30,20 +28,10 @@ public class HttpClientConnection implements WebClient {
     public Publication getPublicationById(Long id) throws IOException {
         String urlAsString = GET_ENDPOINT.concat("/" + id);
         try {
-            HttpRequest httpGetRequest = HttpRequest.newBuilder() // Creating a httpGetRequest object (where to get the data)
-                    .GET()
-                    .header(HttpHeaders.ACCEPT, "application/json")
-                    .header(HttpHeaders.CONTENT_TYPE, "application/json")
-                    .uri(URI.create(urlAsString))
-                    .build();
-            System.out.println("Request Type: " + httpGetRequest.method());  // Printing the request method used
-            HttpClient client = HttpClient.newHttpClient();
-            HttpResponse httpResponse = (HttpResponse) client.send(httpGetRequest, java.net.http.HttpResponse.BodyHandlers.ofString());
-            // Executing the HttpGet request
-            InputStream inputStream = httpResponse.getEntity().getContent(); // Response-stream transformation
-            return mapper.readValue(inputStream, Publication.class); // Stream-PublicationObject transformation
-        } catch (InterruptedException e) {
-            throw new RuntimeException(e);
+            HttpGet getRequest = new HttpGet(urlAsString);
+            System.out.println("Request Type: " + getRequest.getMethod());  // Printing the request method used
+            HttpResponse httpResponse = httpClient.execute(getRequest); // Executing the HttpGet request
+            return mapper.readValue(httpResponse.getEntity().getContent(), Publication.class); // Stream-PublicationObject transformation
         } finally {
             httpClient.close();
         }
@@ -53,6 +41,7 @@ public class HttpClientConnection implements WebClient {
     public Publication postPublication(Publication newPublication, String jsonString) throws IOException {
         if (newPublication.getId() >= 101L) {
             try {
+                HttpPost getRequest = new HttpPost(urlAsString);
                 HttpRequest httpPostRequest = HttpRequest.newBuilder() // Building a httpPostRequest object
                         .header(HttpHeaders.ACCEPT, "application/json")
                         .header(HttpHeaders.CONTENT_TYPE, "application/json")
