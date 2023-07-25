30fcabb46415456d6e2c49ec13ad40a4fe123cd1
AndreiPaliakou <AndreiPaliakouBelarus@gmail.com>
2023-07-26 00:34:41 +0300

    Servlet for Vitya (26.07.2023) (to find out the mistakes)

--- src/main/java/MyServletPage1.java
+++ src/main/java/MyServletPage1.java
@@ -1,25 +1,41 @@
-import javax.servlet.ServletException;
+import javax.servlet.*;
 import javax.servlet.annotation.WebServlet;
-import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import java.io.IOException;
-import java.io.InputStream;
+import javax.servlet.http.*;
+import java.io.*;
 import java.nio.charset.Charset;
+import java.nio.charset.StandardCharsets;
 
 
 @WebServlet(urlPatterns = {"/"})
 public class MyServletPage1 extends HttpServlet {
+
     @Override
     public void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         byte[] result = new byte[64 * 1024];
 
         try (InputStream stream = getClass().getClassLoader().getResourceAsStream("informationToTheServerFromPage1.html")) {
+            assert stream != null;
             stream.read(result);
-
-            String resultAsString = new String(result, Charset.forName("UTF-8"));
-
+            String resultAsString = new String(result, StandardCharsets.UTF_8);
             response.getWriter().print(resultAsString);
         }
     }
+
+    @Override
+    public void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        String checkBox = request.getParameter("nameOfChoice");
+        if (checkBox != null && checkBox.equals("on")) {
+            RequestDispatcher rd = request.getRequestDispatcher("/shopPage2");
+            rd.forward(request, response);
+        } else {
+            byte[] result = new byte[64 * 1024];
+            try (InputStream stream = getClass().getClassLoader().getResourceAsStream("agreementCheckingError.html")) {
+                stream.read(result);
+                String resultAsString = new String(result, StandardCharsets.UTF_8);
+                response.getWriter().print(resultAsString);
+            }
+
+        }
+    }
 }
+
--- src/main/java/MyServletPage2.java
+++ src/main/java/MyServletPage2.java
@@ -3,13 +3,28 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
 
 
 @WebServlet(urlPatterns = {"/shopPage2"})
 public class MyServletPage2 extends HttpServlet {
     @Override
     public void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        HttpSession session = request.getSession();
+
+        StringBuilder orderAsString = new StringBuilder();
+
+        Map<String, Double> productsMap = new HashMap<String, Double>();
+        productsMap.put("Book", 5.5);
+        productsMap.put("Pen", 1.1);
+        productsMap.put("Pencil", 3.3);
+        productsMap.put("Copybook", 2.2);
+
+        String products = request.getParameter("setOfTheProducts");
+        session.setAttribute("products",orderAsString);
         String userName = request.getParameter("userName");
         response.getWriter().println("<!DOCTYPE html>\n" +
                 "<html lang=\"en\">\n" +
@@ -32,6 +47,7 @@ public class MyServletPage2 extends HttpServlet {
                 "    <br>\n" +
                 "    <input type=\"text\" name=\"userName\" value = \"" + userName + "\" hidden>\n" +
                 "    <br>\n" +
+                "<button formaction=\"shopPage2\">Add Item</button>\n"+
                 "     <input type=\"submit\" value=\"ENTER\">\n" +
                 "</form>\n" +
                 "</body>\n" +
